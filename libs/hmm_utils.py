5f9ff85c38076c133c27037f9da9cf32e41dafa2
Hilary Mogul <Hilary.Mogul@gmail.com>
2017-12-02 21:08:08 -0500

    more clear variable naming

--- libs/hmm_utils.py
+++ libs/hmm_utils.py
@@ -18,56 +18,53 @@ def get_move_list(labels):
     return list(zip(init,dest))
 
 def estimate_chord_transitions(chord_mvs):
-    ''' calculate chord movement transitions and priors from transitions '''
-    transitions = np.zeros((7, 7))
+    ''' calculate chord movement transitions and initial probabilities from transitions '''
+    transitions = np.ones((7, 7))
     counter = Counter(chord_mvs)
     for i in range(7):
         for j in range(7):
-            transitions[i,j] = 1 + counter[(i,j)]
+            transitions[i,j] += counter[(i,j)]
     
-    priors = np.sum(transitions, axis=1)
-    transitions /= priors[:,np.newaxis]
-    priors /= np.sum(priors)
-    return transitions,priors
+    initialProbabilities = np.sum(transitions, axis=1)
+    transitions /= initialProbabilities[:,np.newaxis]
+    initialProbabilities /= np.sum(initialProbabilities)
+    return transitions,initialProbabilities
 
 def train_gaussian_models(features, labels, chord_mvs):
-    # a gmm will return the log likelihood of it being in play
-    # essentially representing a binary random variable by a mixture of 
     generic = sklearn.mixture.GaussianMixture(n_components=1,covariance_type='full')
-    print("Features.shape is {}".format(features.shape))
     generic.fit(features)
     models = []
     for chord_index in range(7):
         rows = np.nonzero(labels == chord_index)
 
         if rows:
-            model =  sklearn.mixture.GaussianMixture(n_components=1,covariance_type='full')
+            model = sklearn.mixture.GaussianMixture(n_components=1,covariance_type='full')
             model.fit(features[rows[0]])
             models.append(model)
         else:
             models.append(generic)
     
-    transitions, priors = estimate_chord_transitions(chord_mvs)
-    return models, transitions, priors
+    transitions, initialProbabilities = estimate_chord_transitions(chord_mvs)
+    return models, transitions, initialProbabilities
 
-def estimate_chords(chroma, models, transitions, priors):
+def estimate_chords(chroma, models, transitions, initialProbabilities):
     scoreList = []
     posterior = np.zeros((chroma.shape[0], 7))
     for i in range(chroma.shape[0]):
         for j in range(7):
             posterior[i,j] = models[j].score(chroma[i].reshape(1,-1))
     posterior = np.array(posterior)
-    return viterbi(np.exp(posterior), transitions, priors)
+    return viterbi(np.exp(posterior), transitions, initialProbabilities)
 
-def viterbi(posterior_prob, transition_prob, prior_prob):
-    nFrames = posterior_prob.shape[0]
-    nStates = posterior_prob.shape[1]
+def viterbi(emission_prob, transition_prob, prior_prob):
+    nFrames = emission_prob.shape[0]
+    nStates = emission_prob.shape[1]
     traceback = np.zeros((nFrames, nStates), dtype=int)
-    # best probability of each state
-    best_prob = prior_prob * posterior_prob[0]
+    # best probability of each state, normalized
+    best_prob = prior_prob * emission_prob[0]
     best_prob /= max(0.0001,np.sum(best_prob))
     for f in range(1,nFrames):
-        poss_scores = (transition_prob * np.outer(best_prob, posterior_prob[f]))
+        poss_scores = (transition_prob * np.outer(best_prob, emission_prob[f]))
         traceback[f] = np.argmax(poss_scores, axis=0)
         best_prob = np.max(poss_scores, axis=0)
         best_prob /= max(0.001,np.sum(best_prob))
