3ed9d25f2175a69867baad759c468669a2d4ba78
Hilary Mogul <Hilary.Mogul@gmail.com>
2017-12-02 21:55:55 -0500

    adds end to end for main process

--- create_chords.py
+++ create_chords.py
@@ -6,14 +6,16 @@ import argparse
 import pretty_midi
 import sklearn_crfsuite
 from sklearn import svm
-
+import random
 sys.path.append("libs")
 import pitch_estimation
 import dataset_utils
 import beat_util
 import synth_utils
+import hmm_utils
+
 key_identifier_file = "key_identifier.p"
-chord_predictor_file = "chord_predictor.p"
+hmm_file = "hmm.p"
 parser = argparse.ArgumentParser()
 parser.add_argument("-infile", dest = "infile",help='location of audio file')
 parser.add_argument("-onsetData", dest="onsetfile", help="location of pickle file of onset pattern to use")
@@ -27,20 +29,32 @@ elif args.ismajor.lower() is "true":
     major = True
 else:
     major = False
+
 key_identifier = pickle.load(open(key_identifier_file, "rb"))
+hmm_data = pickle.load(open(hmm_file,"rb"))
 onset_signatures = pickle.load(open(args.onsetfile, "rb"))
+onset_sig = random.choice(onset_signatures)
 # get pitches, chroma stats, and beat sync chroma of the input file
 y,sr = librosa.load(args.infile)
+print("Estimating pitches...")
 pitches = pitch_estimation.calculate_pitches(y,fs=sr)
 midi_notes = pitch_estimation.pitches_to_midi(pitches)
+print("Converting pitches to chroma...")
 beats, all_chroma = pitch_estimation.beat_sync_chroma(data=y, fs=sr, midi_notes)
 chroma_stats = pitch_estimation.data_usage_vector(midi_notes)
 
 # use the chroma stats to get key 
 key = key_identifier.predict(chroma_stats)
 
-# use beat sync chroma to predict chords
+# roll chroma to get the key invariant chroma
+rolled_chroma = np.roll(all_chroma, key, axis=0)
+print("Predicting chords...")
+#hmm = {"models":models, "transitions":transitions, "priors":priors}
+chords = hmm_utils.estimate_chords(rolled_chroma, hmm_data["models"], hmm_data["transitions"], hmm_data["priors"])
 
-# use beat times to make an onset pattern
-
+# get onset times to use
+onsets = synth_utils.onset_signature_to_onsets(onset_sig, beats)
+print("Creating MIDI...")
 # use chord progression, onset pattern, key, and mode to create MIDI output
+mid = synth_utils.create_pretty_midi(chords, beats, onsets, key=key, major=major)
+mid.write(args.outfile)
